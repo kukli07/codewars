50d63f378357522a98b2d904f00646a326ed5a0b
kukli07 <84287747+kukli07@users.noreply.github.com>
2022-10-16 22:52:26 +0200

    Let the games begin

---
+++ aNeedleInTheHaystack.js
@@ -0,0 +1,4 @@
+function findNeedle(haystack) {
+  const index = haystack.findIndex((item) => item === "needle");
+  return `found the needle at position ${index}`;
+}
---
+++ rockPaperScissors.js
@@ -0,0 +1,16 @@
+const rps = (p1, p2) => {
+  if (p1 == p2) return "Draw!";
+
+  if (p1 == "rock" && p2 == "scissors") return "Player 1 won!";
+  else if (p1 == "scissors" && p2 == "paper") return "Player 1 won!";
+  else if (p1 == "paper" && p2 == "rock") return "Player 1 won!";
+  else return "Player 2 won!";
+};
+
+// Clever solution
+// const rps = (p1, p2) => {
+//   if (p1 === p2) return "Draw!";
+//   const rules = { rock: "scissors", paper: "rock", scissors: "paper" };
+//   if (p2 === rules[p1]) return "Player 1 won!";
+//   return "Player 2 won!";
+// };
