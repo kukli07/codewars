6461412dec735796f925e90133ebe976691fa045
kukli07 <84287747+kukli07@users.noreply.github.com>
2022-10-23 10:00:00 +0200

    Math and not meth

---
+++ 6kyu/isANumberPrime.js
@@ -0,0 +1,13 @@
+function isPrime(num) {
+  if (num <= 0 || num === 1) {
+    return false;
+  }
+
+  // Primality test: trial division (did not google solution, just needed to learn maths again)
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (Number.isInteger(num / i)) {
+      return false;
+    }
+  }
+  return true;
+}
