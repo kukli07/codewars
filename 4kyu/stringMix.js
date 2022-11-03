a38e84818e691dee679ecdd649be5766939d9a31
kukli07 <84287747+kukli07@users.noreply.github.com>
2022-11-03 10:49:11 +0100

    something something

--- 4kyu/stringMix.js
+++ 4kyu/stringMix.js
@@ -96,3 +96,28 @@ function mix(s1, s2) {
 
   return result.join("/");
 }
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+function mix(s1, s2) {
+  return alphabet
+    .map((char) => {
+      const s1Count = s1.split("").filter((x) => x === char).length;
+      const s2Count = s2.split("").filter((x) => x === char).length;
+      const maxCount = Math.max(s1Count, s2Count);
+
+      return {
+        char: char,
+        count: maxCount,
+        src: maxCount > s1Count ? "2" : maxCount > s2Count ? "1" : "=",
+      };
+    })
+    .filter((c) => c.count > 1)
+    .sort(
+      (objA, objB) =>
+        objB.count - objA.count ||
+        (objA.src + objA.char > objB.src + objB.char ? 1 : -1)
+    )
+    .map((c) => `${c.src}:${c.char.repeat(c.count)}`)
+    .join("/");
+}
