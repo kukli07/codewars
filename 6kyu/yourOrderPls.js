97752e6afa9d8f55786793d0c62688a81fc3d2ac
kukli07 <84287747+kukli07@users.noreply.github.com>
2022-11-09 10:40:52 +0100

    Pretty proud

--- 6kyu/yourOrderPls.js
+++ 6kyu/yourOrderPls.js
@@ -1,3 +1,25 @@
-function order(words) {
-    // ...
+function compareFn(a, b) {
+    // sort a before b
+    if (
+        // We have to check .isNaN(), because Number(string) returns NaN
+        // on non integers and apparently NaN is typeof number :))
+        +a.split("").find((e) => typeof +e === "number" && !Number.isNaN(+e)) <
+        +b.split("").find((e) => typeof +e === "number" && !Number.isNaN(+e))
+    ) {
+        return -1;
+    }
+    // sort b before a
+    if (
+        +a.split("").find((e) => typeof +e === "number" && !Number.isNaN(+e)) >
+        +b.split("").find((e) => typeof +e === "number" && !Number.isNaN(+e))
+    ) {
+        return 1;
+    }
+    return 0;
 }
+
+function order(words) {
+    return words.split(" ").sort(compareFn).join(" ");
+}
+
+// All the solutions I found on codewars were using regex, which is for cheaters
