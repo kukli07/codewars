ad56fdad729e998cb6701217e6af96c7d388c874
kukli07 <84287747+kukli07@users.noreply.github.com>
2022-11-15 09:11:14 +0100

    damn

---
+++ 8kyu/centuryFromYear.js
@@ -0,0 +1,6 @@
+function century(year) {
+    return year % 100 === 0
+        ? +Math.floor(Number(year / 100)).toFixed(0)
+        : +Math.floor(Number(year / 100)).toFixed(0) + 1;
+}
+// Took me longer than I am willing to admit
