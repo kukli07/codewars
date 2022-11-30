62c67426fa721ea8a2aab351c625b271be1cec53
kukli07 <84287747+kukli07@users.noreply.github.com>
2022-11-30 10:16:56 +0100

    Boolean has toString?

---
+++ 8kyu/booleanToString.js
@@ -0,0 +1,18 @@
+// Parameters: Boolean
+// Returns: String
+
+function booleanToString(b) {
+    // check if b is true, return "true", else "false"
+    return b ? "true" : "false";
+}
+
+// Example:
+// booleanToString(true) // returns "true"
+
+// TIL: Boolean has a toString() method
+// ///-\\\
+// |^   ^|
+// |O   O|
+// |  ~  |
+//  \ O /
+//   | |
