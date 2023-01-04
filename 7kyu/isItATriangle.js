aac02f8e473a729b63546e9e78113742df35a33f
kukli07 <84287747+kukli07@users.noreply.github.com>
2023-01-04 10:43:09 +0100

    new solution

--- 7kyu/isItATriangle.js
+++ 7kyu/isItATriangle.js
@@ -17,3 +17,16 @@ function isTriangle(a, b, c) {
 
 // TIL: The Triangle Inequality Theorem states that the sum of the lengths
 // of any two sides of a triangle must be greater than the length of the third side.
+
+// Easier solution I tough of after I solved it:
+function isTriangle2(a, b, c) {
+    return Math.max(a, b, c) < a + b + c - Math.max(a, b, c);
+}
+
+// The first math.max() determines which side of the triangle is the longest
+// (Basically the "hypotenuse" of the triangle).
+// Then the other two sides have to be longer than the longest side according
+// to the theorem, thus we subtract the longest side from the sum of all sides.
+// Without the restriction that the area should be greater than 0, the comparison
+// would be a "<=" instead of a "<" - so a triangle could be a line, but that
+// would be a non-physical triangle.
