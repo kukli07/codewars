89e717571ae4427fb31bad6a0aa72160a36b5e35
kukli07 <84287747+kukli07@users.noreply.github.com>
2023-02-07 15:22:42 +0100

    Naive String Search

---
+++ 6kyu/stringSearch.js
@@ -0,0 +1,27 @@
+function naiveSearch(str1, str2) {
+    // Initialize the result variable to 0
+    let result = 0;
+
+    // Outer loop to check for each character in str1
+    for (let i = 0; i < str1.length - str2.length + 1; i++) {
+        // Inner loop to check for the occurence of str2 in str1
+        let check = 0;
+        for (let j = 0; j < str2.length; j++) {
+            // Get the current character from str1
+            const char1 = str1[i + j];
+            // Get the current character from str2
+            const char2 = str2[j];
+            // Check if both characters are equal
+            if (char1 === char2) {
+                check++;
+            }
+            // If all the characters of str2 have been matched, increase the result by 1
+            if (check === str2.length) {
+                result++;
+            }
+        }
+    }
+
+    // Return the final result
+    return result;
+}
