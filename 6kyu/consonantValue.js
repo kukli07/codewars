83731cd994966dec0422509a69182660715eeb48
kukli07 <84287747+kukli07@users.noreply.github.com>
2023-05-14 14:00:00 +0200

    Interesting building.

---
+++ 6kyu/consonantValue.js
@@ -0,0 +1,24 @@
+function solve(s) {
+    // Array for vowels
+    const vowels = ["a", "e", "o", "i", "u"];
+    let max = 0;
+    const substrings = s.split(/[aeoiu]/);
+
+    substrings.forEach((substr) => {
+        const groups = substr.split("");
+
+        const value = groups.reduce((prev, curr) => {
+            if (vowels.includes(curr.toLowerCase())) {
+                return prev;
+            }
+
+            return prev + (curr.toLowerCase().charCodeAt(0) - 96);
+        }, 0);
+
+        if (value > max) {
+            max = value;
+        }
+    });
+
+    return max;
+}
